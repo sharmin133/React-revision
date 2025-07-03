396fb3b095d224522d6c0a2eaa4052df7edb9dfa
sharmin133 <mstsharmin1781@gmail.com>
2025-07-03 20:06:15 +0600

    function added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Person></Person>
+    
+    </>
+  )
+}
+
+
+function Person(){
+  return(
+    <h2>i am student</h2>
+  )
+}
+
+export default App
